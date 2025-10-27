1adc1ba06a2490a27ef852cda50c7a8df606c539
Brian García Ortiz <122748240+bgarciaortiz@users.noreply.github.com>
2025-10-26 23:57:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
+#include "lista.h"
 #include "diccionario.h"
+
+    // recorrer
+
+    // destruir hasmap
 
 int main()
 {
-    tDiccionario hasmap[HASHSIZE];
-    crear_dic(hasmap, HASHSIZE);
+    tDiccionario* hasmap=crear_dic(HASHSIZE);
+
+    poner_dic(hasmap,"a","b",1);
 
     return 0;
 }
+
